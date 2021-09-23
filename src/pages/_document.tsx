d3cf58d94c893d31615888dfc9e218e1b1674fa5
JorbFreire <jorbfreire@ufrn.edu.br>
2021-09-22 22:33:19 -0300

    change font url

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,7 +47,7 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap"
             rel="stylesheet"
           />
         </Head>
